c1f897268db0cb5546925439ba35d83cae65c1f4
Josue David Hernandez <josue.d.hernandez.gutierrez@intel.com>
2018-01-22 14:14:56 -0600

    clr-initrd: add DESTDIR to install_initrd.sh
    DESTDIR helps to autospec this source code.
    
    Signed-off-by: Josue David Hernandez <josue.d.hernandez.gutierrez@intel.com>

--- install_initrd.sh
+++ install_initrd.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/bash
 
 INITRD_FILE=$1
-mkdir -p ${MESON_BUILD_ROOT}/initramfs/{proc,dev,sys,root,usr/lib64,lib64}
-cp ${MESON_BUILD_ROOT}/init ${MESON_BUILD_ROOT}/initramfs/
-cp -u /usr/lib64/{libc.so.?,libm.so.?,libgcc_s.so.?,libuuid.so.?,libdevmapper.so.?.??,libgcrypt.so.??,libgpg-error.so.?,libudev.so.?,libpthread.so.?} ${MESON_BUILD_ROOT}/initramfs/usr/lib64/
-cp -u /usr/lib/libcryptsetup.so.? ${MESON_BUILD_ROOT}/initramfs/usr/lib64/
-cp -u /lib64/ld-linux-x86-64.so.? ${MESON_BUILD_ROOT}/initramfs/lib64/
-cd ${MESON_BUILD_ROOT}/initramfs
+mkdir -p ${MESON_BUILD_ROOT}/initramfs/{proc,dev,sys,root,usr/lib64,lib64} && \
+cp ${MESON_BUILD_ROOT}/init ${MESON_BUILD_ROOT}/initramfs/ && \
+cp -u /usr/lib64/{libc.so.?,libm.so.?,libgcc_s.so.?,libuuid.so.?,libdevmapper.so.?.??,libgcrypt.so.??,libgpg-error.so.?,libudev.so.?,libpthread.so.?,libcryptsetup.so.?,ld-linux-x86-64.so.?} ${MESON_BUILD_ROOT}/initramfs/usr/lib64/ && \
+cd ${MESON_BUILD_ROOT}/initramfs && \
 find . -print0 | cpio --null -ov --format=newc | gzip -9 > ${MESON_BUILD_ROOT}/${INITRD_FILE}
-mkdir -p ${MESON_INSTALL_PREFIX}/lib/kernel/
-cp ${MESON_BUILD_ROOT}/${INITRD_FILE} ${MESON_INSTALL_PREFIX}/lib/kernel/${INITRD_FILE}
+mkdir -p ${DESTDIR}/${MESON_INSTALL_PREFIX}/lib/kernel/ && \
+cp ${MESON_BUILD_ROOT}/${INITRD_FILE} ${DESTDIR}/${MESON_INSTALL_PREFIX}/lib/kernel/${INITRD_FILE}
 cd ${MESON_SOURCE_ROOT}
