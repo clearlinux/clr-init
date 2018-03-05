e5490d381f893ae6212f98916a4b4e317c1b4013
Josue David Hernandez <josue.d.hernandez.gutierrez@intel.com>
2018-03-05 10:26:31 -0600

    clr-init: change install path to lib/kernel
    Signed-off-by: Josue David Hernandez <josue.d.hernandez.gutierrez@intel.com>

--- install_initrd.sh
+++ install_initrd.sh
@@ -6,6 +6,6 @@ cp ${MESON_BUILD_ROOT}/init ${MESON_BUILD_ROOT}/initramfs/ && \
 cp -u /usr/lib64/{libc.so.?,libm.so.?,libgcc_s.so.?,libuuid.so.?,libdevmapper.so.?.??,libgcrypt.so.??,libgpg-error.so.?,libudev.so.?,libpthread.so.?,libcryptsetup.so.?,ld-linux-x86-64.so.?} ${MESON_BUILD_ROOT}/initramfs/usr/lib64/ && \
 cd ${MESON_BUILD_ROOT}/initramfs && \
 find . -print0 | cpio --null -ov --format=newc | gzip -9 > ${MESON_BUILD_ROOT}/${INITRD_FILE}
-mkdir -p ${DESTDIR}/${MESON_INSTALL_PREFIX}/lib/initrd.d/ && \
-cp ${MESON_BUILD_ROOT}/${INITRD_FILE} ${DESTDIR}/${MESON_INSTALL_PREFIX}/lib/initrd.d/${INITRD_FILE}
+mkdir -p ${DESTDIR}/${MESON_INSTALL_PREFIX}/lib/kernel/ && \
+cp ${MESON_BUILD_ROOT}/${INITRD_FILE} ${DESTDIR}/${MESON_INSTALL_PREFIX}/lib/kernel/${INITRD_FILE}
 cd ${MESON_SOURCE_ROOT}
